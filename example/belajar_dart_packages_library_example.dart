dabf881160414cc1e8283bfbef909f8e8d783841
Muhammad Maulana Yusuf <mhm.maulanayusuf@gmail.com>
2024-05-09 18:08:06 +0700

    dart library

---
+++ example/belajar_dart_packages_library_example.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_dart_packages_library/belajar_dart_packages_library.dart';
+
+void main() {
+  print(sayHello('Muhammad'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+  print(customer);
+  print(product);
+  print(category);
+}
